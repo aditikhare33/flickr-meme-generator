103b8736780d72fd718b9f9090fc3830971b1a09
aditikh <aditikh@umich.edu>
2021-03-25 17:56:25 -0400

    query works, input text box not functioning properly

--- src/query.js
+++ src/query.js
@@ -4,78 +4,77 @@ class Query extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleSubmit = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+
         this.state = {
             search: "",
             curQuery: "",
             pictures: [],
         };
     }
+
     handleSubmit(e) {
-        e.persist();
+        e.preventDefault();
 
-        console.log("inSubmit");
-        const {curQuery} = this.state;
-
-        let FLICKR_API_KEY = "127eb5c6066070b6ef6203c9d9a89dd8";
-        fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='+FLICKR_API_KEY+'&tags=" + curQuery + "&per_page=10&page=1&format=json&nojsoncallback=1")
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(j){
-                alert(JSON.stringify(j));
-                
-        //loop through all pictures from search
-        let picArray = j.photos.photo.map((pic) => {
-        
-        var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
-        return(
-          <img alt="dogs" src={srcPath}></img>
-        )
+        let {curQuery, search} = this.state;
+        this.setState({
+            search: curQuery,
         })
-
-        this.setState(
-            {pictures: picArray
-        });
-        /*
-        let FLICKR_API_KEY = "127eb5c6066070b6ef6203c9d9a89dd8";
-        var Flickr = require('flickr-sdk');
-        var flickr = new Flickr(process.env.FLICKR_API_KEY);
-        
-        flickr.photos.search({
-            text: 'doggo'
-        }).then(function (res) {
-            console.log('yay!', res.body);
-        }).catch(function (err) {
-            console.error('bonk', err);
-        }); */
-        /*
-        let apiurl = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=" + apikey + "&per_page=10&format=json&nojsoncallback=1";
-        console.log("apiurl", apiurl);
-        */
+        console.log("inSubmit");
+        if (search !== "") {
+            let FLICKR_API_KEY = "127eb5c6066070b6ef6203c9d9a89dd8";
+            fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+ FLICKR_API_KEY +  "&tags=" + search + "&per_page=10&page=1&format=json&nojsoncallback=1")
+                .then((response) => {
+                    if (!response.ok) throw Error(response.statusText);
+                    return response.json();
+                })
+                .then((j) =>{
+                    console.log("IN DATA");
+                    alert(JSON.stringify(j));
+                    //loop through all pictures from search
+                    let picArray = j.photos.photo.map((pic) => {
+                        //copied from flickr's documentation, get location of pictures
+                        var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
+                        return(
+                            <img alt={search} src={srcPath}></img>
+                        )
+                    })
+                    this.setState({ 
+                        pictures: picArray,
+                    });
+                })
+                .then((error) => {
+                    console.log("ERROR:", error);
+                })
+        }   
     }
 
     handleChange(e) {
         console.log("inChange"); 
-        e.persist();
-        this.setState({
-            curQuery: e.target.value,
-          });
+        e.preventDefault();
+        if (e.target.value !== "") {
+            this.setState({
+                curQuery: e.target.value,
+            });
+        }
     }
+
     render() {
-        const {curQuery, pictures} = this.state;
+        const {curQuery, pictures, search} = this.state;
+        console.log(this.state);
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="q" onChange={this.handleChange} value={curQuery}/>
-                    <input type="submit" value="Search Images" />
-                </form>
+                <div>
+                    <form>
+                        <input type="text" onChange={this.handleChange} value={curQuery}/>
+                        <button onClick={this.handleSubmit} type="submit"> Search! </button>
+                    </form>
+                </div>
+                <div>
+                    Pictures of {curQuery}: {pictures}
+                </div>
             </div>
-            <div>
-                {pictures}
-            </div>
-        
         );
     }
 }
